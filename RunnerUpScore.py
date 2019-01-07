da36fddf7b061d0c5074f8921674466215252bd6
Jasleen <jasleen.pec@gmail.com>
2019-01-07 08:06:00 -0600

    Add files via upload

---
+++ RunnerUpScore.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  7 07:53:00 2019
+
+@author: jasleenarora
+"""
+
+## Problem
+# Given the participants' score sheet for your University Sports Day, 
+# you are required to find the runner-up score. 
+# You are given n scores. Store them in a list and find the score of the runner-up.
+
+'''Sample Input 0
+5
+2 3 6 6 5'''
+
+'''Sample Output 0
+5'''
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    z = max(arr)
+    while max(arr) == z:
+        arr.remove(max(arr))
+
+    print (max(arr))
